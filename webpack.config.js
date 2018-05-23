7e99d885e3600cd266a0979c7e57efbd112ab471
zhangguobiao <zhangguobiao@huxiu.com>
2018-05-23 16:07:01 +0800

    webpack 第四节

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,29 @@
 const path = require('path');
 module.exports = {
+    //入口配置
     entry: {
         entry: './src/entry.js'
     },
+    //出口配置
     output: {
         //node的知识
         path: path.resolve(__dirname,'dist'),
         filename: 'bundle.js'
     },
+    //依赖包
     module: {
 
     },
+    //插件
     plugins: [],
     devServer: {
-
+        //设置基本目录结构
+        contentBase:path.resolve(__dirname,'dist'),
+        //服务器的IP地址
+        host: 'localhost',
+        //服务端压缩是否开启
+        compress: true,
+        //配置服务端口号
+        port: 1717
     }
 }
